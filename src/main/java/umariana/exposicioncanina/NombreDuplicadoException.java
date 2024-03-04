ef5894b92626cf6f5bc6b99ffb3fb83c3b549908
Afmunozz <Anfelipemu47@gmail.com>
2024-03-04 16:14:13 -0500

    Proyecto de exposicion caninca trabajada en clase apo 3

---
+++ src/main/java/umariana/exposicioncanina/NombreDuplicadoException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package umariana.exposicioncanina;
+
+/**
+ *
+ * @author Alexander Lopez 
+ */
+public class NombreDuplicadoException extends  Exception {
+
+    
+    public  NombreDuplicadoException(){
+    super("Ya existe un perro con ese nombre, por favor vuelve a intentarlo ");
+}
+
+  
+    
+    
+}
